d739fd90412e14598581f408867ff6760059a1f9
Tobias Brunsch <tobias.brunsch@aol.de>
2020-01-12 23:56:56 +0100

    MOD TestUtils: Removed unnecessary polling in sleep method

--- hippodamus-tests/test/dd/kms/hippodamus/testUtils/TestUtils.java
+++ hippodamus-tests/test/dd/kms/hippodamus/testUtils/TestUtils.java
@@ -6,23 +6,12 @@ public class TestUtils
 {
 	public static final boolean[]	BOOLEANS	= { false, true };
 
-	private static final long	SLEEP_INTERVAL_MS	= 100;
-
 	public static void sleep(long timeMs) {
-		long end = System.currentTimeMillis() + timeMs;
 		try {
-			do {
-				Thread.sleep(SLEEP_INTERVAL_MS);
-				if (Thread.interrupted()) {
-					throw new InterruptedException();
-				}
-			} while (System.currentTimeMillis() < end);
+			Thread.sleep(timeMs);
 		} catch (InterruptedException e) {
-			try {
-				Thread.sleep(SLEEP_INTERVAL_MS);
-			} catch (InterruptedException ignored) {
-
-			}
+			Thread.currentThread().interrupt();
+			throw new IllegalStateException("Unexpected thread interruption", e);
 		}
 	}
 
