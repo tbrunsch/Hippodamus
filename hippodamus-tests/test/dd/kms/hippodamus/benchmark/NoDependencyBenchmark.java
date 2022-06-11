34e5fde27240dcec13261bd7c7540b6e99b77041
Tobias Brunsch <tobias.brunsch@aol.de>
2022-06-12 00:06:16 +0200

    Reformatted comments of benchmark classes

--- hippodamus-tests/test/dd/kms/hippodamus/benchmark/NoDependencyBenchmark.java
+++ hippodamus-tests/test/dd/kms/hippodamus/benchmark/NoDependencyBenchmark.java
@@ -15,10 +15,9 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 /**
- * One of the main purposes of {@link ExecutionCoordinator}s is
- * handling exceptions and dependencies. If there are no dependencies, then other approaches
- * with less overhead may be a better alternative. However, we want to ensure that the
- * framework overhead is not too large.
+ * One of the main purposes of {@link ExecutionCoordinator}s is handling exceptions and dependencies. If there are no
+ * dependencies, then other approaches with less overhead may be a better alternative. However, we want to ensure that
+ * the framework overhead is not too large.
  */
 class NoDependencyBenchmark
 {
