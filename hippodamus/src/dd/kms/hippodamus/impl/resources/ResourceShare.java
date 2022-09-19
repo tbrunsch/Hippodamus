2b4a2418f650600d34de313046bf6f73b57d1ce8
Tobias Brunsch <tobias.brunsch@aol.de>
2022-09-19 21:51:08 +0200

    ResourceShare: Documented expected behavior

--- hippodamus/src/dd/kms/hippodamus/impl/resources/ResourceShare.java
+++ hippodamus/src/dd/kms/hippodamus/impl/resources/ResourceShare.java
@@ -4,8 +4,24 @@ import dd.kms.hippodamus.api.resources.ResourceRequestor;
 
 public interface ResourceShare
 {
+	/**
+	 * Adds the resource share to the pending resource shares. It is guaranteed that
+	 * this method is not called twice without a call to {@link #removePendingResourceShare()}
+	 * inbetween.
+	 */
 	void addPendingResourceShare();
+
+	/**
+	 * Removes the resource share from the pending resource shares if it has been added before
+	 * and not yet removed. Otherwise, calling this method does not have any effect.
+	 */
 	void removePendingResourceShare();
+
+	/**
+	 * Tries to acquire the resource share. It is guaranteed that this method is not called
+	 * a second time after a successful call to this method without a call to {@link #release()}
+	 * before.
+	 */
 	boolean tryAcquire(ResourceRequestor resourceRequestor);
 
 	/**
@@ -14,5 +30,9 @@ public interface ResourceShare
 	 */
 	void release();
 
+	/**
+	 * Removed the specified {@code resourceRequestor} from the underlying resource.
+	 * Calling this method multiple times should not have any effect.
+	 */
 	void remove(ResourceRequestor resourceRequestor);
 }
